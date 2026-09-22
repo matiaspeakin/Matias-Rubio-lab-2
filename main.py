d670f3b8aaabe1008a654cb0d4402ba69369f772
matiaspeakin <mrubio29@setoncatholic.org>
2026-09-22 20:10:00 +0000

    finish code

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-x = 5
-if x > 5:
-    print("hello")
-elif x > 5:
-    print("goodbye")
-else:
-    print ("stinky")
+import random
+
+
+game_number = random.randint(1,10)
+print(game_number)
+
+while( True):
+
+    user_guess = int(input("guess a number between 1 and 10: "))
+
+
+    if user_guess > game_number:
+        print("yo your stright booty but checks its too high")
+    elif user_guess < game_number:
+        print("yo your stright booty but checks its too low")
+    else:
+        print("good job you guessed the number")
+        break
